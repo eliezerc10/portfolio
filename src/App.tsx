e72d0929b9561bf2509ea0492cca47aff5b84953
Eliezer Castillo <eliezercastillo6@gmail.com>
2024-06-13 22:52:59 -0400

    Bug fixed with location ancor

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   const contactData = [
     { icon: 'call', content: '+58 412-9922602', url: 'https://api.whatsapp.com/send?phone=584129922602'},
     { icon: 'email', content: 'eliezercastillo6@gmail.com', url: 'mailto:eliezercastillo6@gmail.com?subject=Full-stack Web Developer' },
-    { icon: 'location_on', content: 'Caracas, Venezuela', url: ' '}
+    { icon: 'location_on', content: 'Caracas, Venezuela', url: ''}
   ]
   const aboutLinks = [
     {
