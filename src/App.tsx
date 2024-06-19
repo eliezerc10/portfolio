8f1b0a27ea887749bca7d8cb45ac7d4a6a82cb1f
Eliezer Castillo <eliezercastillo6@gmail.com>
2024-06-18 20:47:45 -0400

    Improvement: I added lazy loading to wait for images, and added loading component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,54 @@
 import './App.css';
-import { About } from './components/about/About';
-import { Navbar } from './components/navbar/Navbar';
-import { Skills } from './components/skills/Skills';
-import { Experience } from './components/experience/Experience';
-import { Contact } from './components/contact/Contact';
-import { Regards } from './components/regards/Regards';
+
 import { links } from './data/links';
 import { contactData } from './data/contactData';
 import { aboutLinks } from './data/aboutLinks';
 import { skills } from './data/skills';
 import { experiences } from './data/experiences';
+import { Suspense, lazy } from 'react';
+import { Loading } from './components/loading/Loading';
 
 function App() {
 
+  const Navbar = lazy(() =>
+    import('./components/navbar/Navbar')
+      .then(({ Navbar }) => ({ default: Navbar })),
+  )
+
+  const About = lazy(() =>
+    import('./components/about/About').then(({ About }) => ({ default: About })),
+  );
+
+  const Skills = lazy(() =>
+    import('./components/skills/Skills').then(({ Skills }) => ({ default: Skills })),
+  );
+
+  const Experience = lazy(() =>
+    import('./components/experience/Experience').then(({ Experience }) => ({ default: Experience })),
+  );
+
+  const Contact = lazy(() =>
+    import('./components/contact/Contact').then(({ Contact }) => ({ default: Contact })),
+  );
+
+  const Regards = lazy(() =>
+    import('./components/regards/Regards').then(({ Regards }) => ({ default: Regards })),
+  );
+
   return (
     <>
-      <header>
-        <Navbar links={links} />
-      </header>
-      <main>
-        <About aboutLinks={aboutLinks}/>
-        <Skills skills={skills} />
-        <Experience experiences={experiences} />
-        <Contact contactData={contactData} aboutLinks={aboutLinks}/>
-        <Regards message='Your time is valuable, thank you for sharing it!'/>
-      </main> 
-      
+      <Suspense fallback={<Loading />}>
+        <header>
+          <Navbar links={links} />
+        </header>
+        <main>
+          <About aboutLinks={aboutLinks} />
+          <Skills skills={skills} />
+          <Experience experiences={experiences} />
+          <Contact contactData={contactData} aboutLinks={aboutLinks} />
+          <Regards message='Your time is valuable, thank you for sharing it!' />
+        </main>
+      </Suspense>
     </>
   );
 }
--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,11 +1,14 @@
+import '../loading/loading.css'
+
 interface LoadingProps {
 
 }
 
-export const Loading: React.FC<LoadingProps> = ({}) => {
-    return(
-        <div className="loader-container">
-          <div className="loader">Loading...</div>
+export const Loading: React.FC<LoadingProps> = ({ }) => {
+    return (
+        <div className="loading">
+            <div className="spinner"></div>
+            <p>Loading...</p>
         </div>
     )
 }
