e58ee68558ad4632a962b7ba426bf5379cdb2fac
Eliezer Castillo <eliezercastillo6@gmail.com>
2026-02-28 15:55:48 -0400

    Updates

---
+++ assets/Regards-DAZBz9UF.js
@@ -0,0 +1,2 @@
+import{u as t,j as s}from"./react-vendor-DSWkE7wl.js";const a=()=>{const{t:e}=t();return s.jsx("section",{className:"regards-section",children:s.jsx("h2",{className:"bottom-title",children:e("regards.message")})})};export{a as Regards};
+//# sourceMappingURL=Regards-DAZBz9UF.js.map
