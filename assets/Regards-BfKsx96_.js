9298fa78db7ea21f43eb85d7cdf6cfd2e5368b6a
Eliezer Castillo <eliezercastillo6@gmail.com>
2026-02-28 14:48:10 -0400

    Updates

---
+++ assets/Regards-BfKsx96_.js
@@ -0,0 +1,2 @@
+import{j as s}from"./react-vendor-5BLqC-VU.js";const r=({message:e})=>s.jsx("section",{className:"regards-section",children:s.jsx("h2",{className:"bottom-title",children:e})});export{r as Regards};
+//# sourceMappingURL=Regards-BfKsx96_.js.map
